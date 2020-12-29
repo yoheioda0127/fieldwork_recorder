abec3e7f589c41f3f9c420f41c1f664fb4ac689e
yoheioda0127 <maikemi00romans@gamil.com>
2020-12-29 12:15:58 +0900

    articleテーブルの作成

--- db/migrate/20201229025531_create_articles.rb
+++ db/migrate/20201229025531_create_articles.rb
@@ -1,7 +1,14 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-
+      t.integer    :user_id          , foreign_key: true
+      t.integer    :major_id         , foreign_key: true
+      t.string     :title            , null: false
+      t.date       :activity_date    , null: false
+      t.integer    :weather_id       , null: false
+      t.string     :location
+      t.text       :appeal_point
+      t.text       :memo             , null: false
       t.timestamps
     end
   end

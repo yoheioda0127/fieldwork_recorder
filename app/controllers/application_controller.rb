3a3edbb13c49f7e92b39e018ba1c9a701fc3710d
yoheioda0127 <maikemi00romans@gamil.com>
2020-12-29 13:53:18 +0900

    application controllerへの記載

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:namename, :email, :encrypted_password, :birthday, :school_and_campany, :major_id, :research_theme, :introduction])
+  end
 end

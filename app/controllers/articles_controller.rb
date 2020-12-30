163f4b5a07836b0734c87714f65a6d14d5dc5717
yoheioda0127 <maikemi00romans@gamil.com>
2020-12-30 09:29:45 +0900

    トップページ投稿記事へのuser.nameの表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,10 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    Article.includes(:user).order("created_at DESC")
     @articles = Article.all
     @users = User.all
+
   end
 
 end

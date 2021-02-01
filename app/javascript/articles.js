f8f97ef95da157be7ccfdfccd392de4839bcb3a5
yoheioda0127 <maikemi00romans@gamil.com>
2021-02-02 05:59:44 +0900

    top画像へのswiperの実装

--- app/javascript/articles.js
+++ app/javascript/articles.js
@@ -1,15 +1,16 @@
-const pics_src = ["images/slider-1.jpg","images/slider-2.jpg","images/slider-3.jpg","images/slider-4.jpg","images/slider-5.jpg"];
-let num = -1;
+// const pics_src = ["images/slider-1.jpg","images/slider-2.jpg","images/slider-3.jpg","images/slider-4.jpg","images/slider-5.jpg"];
+// let num = -1;
 
 
-function slideshow_timer(){
-  if (num === 4){
-    num = 0;
-  } 
-  else {
-    num ++;
-  }
-  document.getElementById("slider").src = pics_src[num];
-}
+// function slideshow_timer(){
+//   if (num === 4){
+//     num = 0;
+//   } 
+//   else {
+//     num ++;
+//   }
+//   document.getElementById("slider").src = pics_src[num];
+// }
 
-setInterval(slideshow_timer, 3000);
+// setInterval(slideshow_timer, 3000);
+

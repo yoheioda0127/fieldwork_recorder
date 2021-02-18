1e529d7214e54db1514527fc8ad39f63033d405b
yoheioda0127 <maikemi00romans@gamil.com>
2021-02-18 19:03:16 +0900

    フォロー機能のルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles
   resources :users, only: [:show, :edit, :update]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end

baa8196fe670e28d9cb9c01013b97a89b90e873b
yoheioda0127 <maikemi00romans@gamil.com>
2021-02-22 18:43:50 +0900

    フォロー機能実装（ボタンまで）

---
+++ app/controllers/follow_relationships_controller.rb
@@ -0,0 +1,27 @@
+class FollowRelationshipsController < ApplicationController
+  def create
+    @user =User.find(params[:follow_relationship][:following_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html {redirect_back(fallback_location: root_url)}
+      format.js
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:follow_relationship][:following_id])
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html {redirect_back(fallback_location: root_url)}
+      format.js
+    end
+  end
+  def create
+    @user =User.find(params[:follow_relationship][:following_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user, flash: {success: 'フォローしました！'} }
+      format.js
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,17 +25,17 @@ class User < ApplicationRecord
 
     # ユーザーをフォローする
     def follow(other_user)
-      following << other_user
+      self.following << other_user
     end
   
     # ユーザーをフォロー解除する
     def unfollow(other_user)
-      active_relationships.find_by(followed_id: other_user.id).destroy
+      self.active_relationships.find_by(followed_id: other_user.id).destroy
     end
   
     # 現在のユーザーがフォローしてたらtrueを返す
     def following?(other_user)
-      following.include?(other_user)
+      self.following.include?(other_user)
     end
 
 end

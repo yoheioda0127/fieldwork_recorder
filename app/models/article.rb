67ff9049d7e77e183f86ce1270fbd524ebf9edf0
yoheioda0127 <maikemi00romans@gamil.com>
2021-02-27 10:42:31 +0900

    Articleテストコード追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,14 +13,14 @@ class Article < ApplicationRecord
 
   validates :top_image,    presence: {message: 'は必須内容です。' }
 
-  validates :title,        presence: true, length: {maximum: 30}
+  validates :title,        presence: true, length: {maximum: 30, message: 'は30文字以内です。'}
 
-  validates :weather_id,   presence: true, numericality: { other_than: 1 }
+  validates :weather_id,   presence: true, numericality: { other_than: 1 },inclusion: {in: 1..15 }
 
-  validates :location,     length: {maximum: 45}
+  validates :location,     length: {maximum: 45, message: 'は45文字以内です。'}
 
-  validates :appeal_point, length: {maximum: 120}
+  validates :appeal_point, length: {maximum: 120, message: 'は120文字以内です。'}
 
-  validates :memo,         length: {maximum: 170}
+  validates :memo,         length: {maximum: 170, message: 'は170文字以内です。'}
 
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -28,19 +28,46 @@ describe Article do
     # 異常系↓
     context "articleが保存できない場合" do
       it "titleがないとarticleは保存できない" do
+        article = FactoryBot.build(:article)
         @article.title = nil
         @article.valid?
         expect(@article.errors.full_messages).to include("Titleを入力してください")
       end
       it "top_imageがないとarticleは保存できない" do
+        article = FactoryBot.build(:article)
         @article.top_image = nil
         @article.valid?
         expect(@article.errors.full_messages).to include("Top imageは必須内容です。")
       end
       it "weather_idがないとarticleは保存できない" do
+        article = FactoryBot.build(:article)
         @article.weather_id = nil
         @article.valid?
         expect(@article.errors.full_messages).to include("Weatherは数値で入力してください")
+      end
+      it "titleが31文字だとarticleは保存できない" do
+        article = FactoryBot.build(:article)
+        @article.title = 'あああああああああああああああああああああああああああああああ'
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Titleは30文字以内です。")
+      end
+      it "locationが46文字だとarticleは保存できない" do
+        article = FactoryBot.build(:article)
+        @article.location = 'ああああああああああああああああああああああああああああああああああああああああああああああ'
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Locationは45文字以内です。")
+      end
+      it "memoが171文字だとarticleは保存できない" do
+        article = FactoryBot.build(:article)
+        @article.memo = 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Memoは170文字以内です。")
+      end
+      it "appeal_pointが121文字だとarticleは保存できない" do
+        article = FactoryBot.build(:article)
+        @article.appeal_point = 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ'
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Appeal pointは120文字以内です。")
       end
     end
   end

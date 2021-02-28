8ea01cce49c0e7c237c6e743cdc40ac47e22ebb9
yoheioda0127 <maikemi00romans@gamil.com>
2021-02-28 21:07:41 +0900

    ベーシック認証のコメントアウト外し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :basic_auth
+  before_action :basic_auth
 
   def application
     @article = Article.find(params[:id])
@@ -16,9 +16,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :email, :encrypted_password, :school_and_company, :major_id, :research_theme, :introduction])
   end
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
+    end
+  end
 end
